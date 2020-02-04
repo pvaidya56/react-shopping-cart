e14a0356c4b6b5d1eade810fecbfcf708a6404ec
Priya <pvaidya56@gmail.com>
2020-02-03 18:46:20 -0800

    Products componets is getting data from context api

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 	};
 
 	return (
-		<ProductContext.Provider>
+		<ProductContext.Provider value={{ products, addItem}}>
 			<div className="App">
 				<Navigation cart={cart} />
 
@@ -27,12 +27,7 @@ function App() {
 				<Route
 					exact
 					path="/"
-					render={() => (
-						<Products
-							products={products}
-							addItem={addItem}
-						/>
-					)}
+					component={Products}
 				/>
 
 				<Route
